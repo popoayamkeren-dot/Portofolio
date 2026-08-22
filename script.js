bc6d0dcd5a2cd0ceb84c4b9d46c6aa6e3b681074
popoayamkeren-dot <popoayamkeren@gmail.com>
2026-08-22 20:20:25 +0700

    script.js

--- script.js
+++ script.js
@@ -31,7 +31,79 @@ if (menuBtn) {
 
 }
 
+// =========================
+// STAR
+// =========================
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
 
+let stars = [];
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
+
+// Membuat bintang
+function createStars() {
+    stars = [];
+
+    for (let i = 0; i < 120; i++) {
+        stars.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            size: Math.random() * 2 + 0.5,
+            speedX: (Math.random() - 0.5) * 0.4,
+            speedY: (Math.random() - 0.5) * 0.4,
+            opacity: Math.random()
+        });
+    }
+}
+
+createStars();
+
+
+// Animasi bintang
+function animateStars() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    stars.forEach(star => {
+
+        star.x += star.speedX;
+        star.y += star.speedY;
+
+        // Kalau keluar layar, muncul lagi dari sisi lain
+        if (star.x < 0) star.x = canvas.width;
+        if (star.x > canvas.width) star.x = 0;
+
+        if (star.y < 0) star.y = canvas.height;
+        if (star.y > canvas.height) star.y = 0;
+
+        ctx.beginPath();
+
+        ctx.arc(
+            star.x,
+            star.y,
+            star.size,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fillStyle = `rgba(255, 255, 255, ${star.opacity})`;
+
+        ctx.fill();
+    });
+
+    requestAnimationFrame(animateStars);
+}
+
+animateStars();
 // =========================
 // CLOSE MENU AFTER CLICK
 // =========================
